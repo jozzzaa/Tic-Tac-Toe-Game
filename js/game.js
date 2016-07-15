a16fd9fe2aa85699f3239ec32aa5ad5a66001b39
Jordan Etzler <jozza24@gmail.com>
2016-07-15 14:43:35 +1000

    Tic Tac Toe Presentation

--- js/game.js
+++ js/game.js
@@ -1,51 +1,39 @@
 //// WELCOME TO TIC TAC TOE JAVASCRIPT ////
 
 
-//store in local storage
-//http://stackoverflow.com/questions/3357553/how-to-store-an-array-in-localstorage
-//https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+//Player Names
+var player1a = '';
+var player2a = '';
 
-// var names = [];
-// names[0] = prompt("New member name?");
-// localStorage.setItem("names", JSON.stringify(names));
-//
-// //...
-// var storedNames = JSON.parse(localStorage.getItem("names"));
-
-
-///////////////////////////
-
-
-
-var gridSize = 3;
 
 //Box ID Position
-var x = 0;
-var y = 0;
+var x = '';
+var y = '';
 
-//Board Variables + local storage
+
+//Board Variables
 var board = [];
+
 
 //Winning Arrays
 var winners = [];
-
-function winningCombinations() {
+function winningCombinations(gridSize) {
 
   //Horizontal Rows
   winners = [];
-  for (var c = 0; c <= gridSize + 1; c++) {   // second number 0.x
+  for (var c = 0; c <= gridSize - 1; c++) {
     var winnerRow = [];
-    for(var d = 0; d <= gridSize + 1; d++) { // first number x.0 each time
-      winnerRow.push(d + '.' + c);
+    for(var d = 0; d <= gridSize - 1; d++) {
+      winnerRow.push(c + '.' + d);
     }
     winners.push(winnerRow);
   }
 
   //Vertical Rows
-  for (var e = 0; e <= gridSize + 1; e++) {
+  for (var e = 0; e <= gridSize - 1; e++) {
     var winnerColumn = [];
-    for (var f = 0; f <= gridSize + 1; f++) {
-      winnerColumn.push(e + '.' + f);
+    for (var f = 0; f <= gridSize - 1; f++) {
+      winnerColumn.push(f + '.' + e);
     }
     winners.push(winnerColumn);
   }
@@ -55,19 +43,16 @@ function winningCombinations() {
   var rightDiagonal = [];
   for(var g = 0; g <= (gridSize - 1); g++) {
     leftDiagonal.push( g + '.' + g );
-    rightDiagonal.push( ((gridSize - 1) - g) + '.' + g )
+    rightDiagonal.push( ((gridSize - 1) - g) + '.' + g );
   }
   winners.push(leftDiagonal);
   winners.push(rightDiagonal);
 
 }
 
-winningCombinations();
-
 
 //Who goes first? - Player 1 or Player 2
 var player1 = true;
-
 function whosTurn () {
   var randomDecimal = Math.random();
   var randomNumber = Math.round(randomDecimal);
@@ -80,23 +65,21 @@ function whosTurn () {
   }
 }
 
-whosTurn();
+whosTurn();//Causing errors when moving buildGrid
 
 
 //Grid Build / Sizing / HTML
 function buildGrid (gridSize) {
   var boxHolder = $('.canvas');
   for (var k = 0; k <= gridSize - 1; k++) {
-    for (var i = 0; i <= gridSize - 1; i++) {
-      var box = $('<div>').attr('id', (k + '.' + i)).addClass('box');
+    for (var l = 0; l <= gridSize - 1; l++) {
+      var box = $('<div>').attr('id', (k + '.' + l)).addClass('box');
       box.appendTo(boxHolder);
     }
   }
   var boxSize = ((100/gridSize));
   $('.box').css({width: boxSize + '%', height: boxSize + '%' });
 }
-
-buildGrid(gridSize);
 
 
 //Grid Build DOM / Arrays
@@ -112,40 +95,8 @@ function gridDOM() {
   board = shell;
 }
 
-gridDOM();
-
-
-//Box Style Changes on Click
-$('.box').on('click', function (event) {
-  if (player1 === true) {
-      var divID = event.target.id;
-      getVectors(divID);
-      board[x][y] = 'x';
-      domToHtmlBoard();
-      $('#player1').css({ 'opacity': '0.7'});
-      $('#player2').css({ 'opacity': '1'});
-      player1 = false;
-  } else if (player1 === false) {
-      var divID2 = event.target.id;
-      getVectors(divID2);
-      board[x][y] = 'o';
-      domToHtmlBoard();
-      $('#player2').css({ 'opacity': '0.7'});
-      $('#player1').css({ 'opacity': '1'});
-      player1 = true;
-  }
-});
-
-
-//Get HTML Div Vectors/ID as Number
-function getVectors (num) {
-  x = Math.floor(num);
-  y = parseFloat(num - Math.floor(num)).toFixed(1) * 10;
-}
-
 
 //Translate arrays to live board
-
 function domToHtmlBoard () {
 
   for ( var h = 0; h <= (gridSize - 1); h++) {
@@ -157,7 +108,6 @@ function domToHtmlBoard () {
         $(document.getElementById(current)).css({'background-size': '70% 70%'});
         $(document.getElementById(current)).css({'background-repeat': 'no-repeat'});
         $(document.getElementById(current)).css({'background-position': 'center'});
-        // winValidator();
       } else if (board[h][g] === 'o'){
         var current2 = h + '.' + g;
         $(document.getElementById(current2)).css({'background-image': 'url("images/hugs.png")'});
@@ -165,164 +115,146 @@ function domToHtmlBoard () {
         $(document.getElementById(current2)).css({'background-size': '70% 70%'});
         $(document.getElementById(current2)).css({'background-repeat': 'no-repeat'});
         $(document.getElementById(current2)).css({'background-position': 'center'});
-        // winValidator();
       }
     }
   }
 }
 
 
+//Play Now Button
+var gridSize = '3';
+
+$('.playNow').on('click', function() {
+
+  gridSize = $('.boardSize').val();
+  buildGrid(gridSize);
+  gridDOM();
+
+  if ( $('.Player1Name').val() === "") {
+    player1a = "Player 1";
+    $('#player1 > div > h3').html(player1a);
+  } else {
+    player1a = $('.Player1Name').val();
+    $('#player1 > div > h3').html(player1a);
+  }
+
+  if ( $('.Player2Name').val() === "") {
+    player2a = "Player 2";
+    $('#player2 > div > h3').html(player2a);
+  } else {
+    player2a = $('.Player2Name').val();
+    $('#player2 > div > h3').html(player2a);
+  }
+
+  $('.wrapperIntro').css({ 'display': 'none'});
+  $('.canvas').css({ 'display': 'block'});
+  $('.gameStats').css({ 'display': 'block'});
 
 
+});
+
+winningCombinations(gridSize);
+gridDOM();
+domToHtmlBoard();
+
+
+//Box Style Changes on Click
+$('.canvas').on('click', '.box', function (event) {
+  if (player1 === true) {
+      var divID = event.target.id;
+      getVectors(divID);
+      board[x][y] = 'x';
+      domToHtmlBoard();
+      $('#player1').css({ 'opacity': '0.7'});
+      $('#player2').css({ 'opacity': '1'});
+      player1 = false;
+
+  } else if (player1 === false) {
+      var divID2 = event.target.id;
+      getVectors(divID2);
+      board[x][y] = 'o';
+      domToHtmlBoard();
+      $('#player2').css({ 'opacity': '0.7'});
+      $('#player1').css({ 'opacity': '1'});
+      player1 = true;
+
+  }
+
+  if (checkWinnerX() === true) {
+    $('.announcementHolderX').css({ 'transition': 'all 2s'});
+    $('#xWins').html(player1a + " Wins with X's")
+    $('.announcementHolderX').css({ 'display': 'block'});
+    $('.announcementHolderX').css({ 'opacity': '1'});
+  }
+
+  if (checkWinnerO() === true) {
+    $('.announcementHolder0').css({ 'transition': 'all 2s'});
+    $('#oWins').html(player2a + " Wins with O's")
+    $('.announcementHolder0').css({ 'display': 'block'});
+    $('.announcementHolder0').css({ 'opacity': '1'});
+  }
+
+});
+
+
+//Get HTML Div Vectors/ID as Number
+function getVectors(num) {
+    x = Math.floor(num);
+    y = parseFloat(num - Math.floor(num)).toFixed(1) * 10;
+
+    return [x,y];
+}
+
+function getVectors2(num) {
+    var x = Math.floor(num);
+    var y = parseFloat(num - Math.floor(num)).toFixed(1) * 10;
+
+    return [x,y];
+}
 
 
 //Scoring
+//Thank you Harry!
+function checkWinnerX() {
+    var win = false;
+    winners.some(function(element, i){
+        win = element.every(function(f3) {
+            var coord = getVectors2(f3);
+            if (board[coord[0]][coord[1]] === "x") {
+              return true;
+            }
 
+        });
+        return win;
+    });
+    return win;
+}
 
-//if all arrays have either an x or an o, then tie add this to an an if statement at the end for else
-//
-// for (var m = 0; m <= winners; m++) {
-//   winners[m]
-// }
-//
-//
-//
-//
-//
-// getVectors(1.1)
-// //x and y variables
-// indexOf(board[x][y]) === 'x' && indexOf(getVectors(1.1)) === 'o'
-//
-//
-// take array 1
-// get coordinates from each spot
-// if all equal x ==>
-// if all equal o ==>
-// else ==>continue
-//
-// function winValidator() {
-//   var successes = [];
-//   for (var n = 0; n <= gridSize - 1; n++) {
-//     for (var m = 0; m <= gridSize - 1; m++) {
-//       var coordinate = winners[m][n];
-//       var cY = Math.floor(coordinate);
-//       var cX = parseFloat(coordinate - Math.floor(coordinate)).toFixed(1) * 10;
-//       if (board[cY][cX] === 'x') {
-//         successes.push('x');
-//       } else if (board[cY][cX] === 'o') {
-//         successes.push('o');
-//       }
-//       console.log(coordinate);
-//       console.log(cX);
-//       console.log(cY);
-//       console.log(successes);
-//       // debugger
-//     }
-//     var winOrNo = arraySame(successes);
-//     console.log(winOrNo);
-//     if (winOrNo = true) {
-//       console.log('We have a winner');
-//     } else {
-//       successes = [];
-//     }
-//   }
-// }
-
-
-//Check Successes array is identical
-function arraySame(successes) {
-    for(var i = 1; i < successes.length; i++) {
-      if(successes[0] !== successes[i]) {
-        return false;
-      }
-    }
-  return true;
+function checkWinnerO() {
+    var win2 = false;
+    return winners.some(function(element2, i2){
+          var a = element2.every(function(f2) {
+            var coord2 = getVectors2(f2);
+            if (board[coord2[0]][coord2[1]] === "o") {
+              return true;
+            } else {
+              return false;
+            }
+        });
+      return a;
+    });
 }
 
 
+//Functionality to convey a draw
+function drawMaybe() {
+  for(var aa = 0; aa <= gridSize - 1; aa++) {
 
-
-
-function winValidator() {
-
-  // 00 01 02
-  // 10 11 12
-  // 20 21 22
-
-
-  // //horizontal
-  // //column doesnt grow, row grows
-  // if board[0][0] === x && board[0][1] === x && board[0][2] === x
-  // if board[1][0] === x && board[1][1] === x && board[1][2] === x
-  // if board[2][0] === x && board[2][1] === x && board[2][2] === x
-
-
-
-  for (n = 0; n <= gridSize - 1; n++) {
-    for (var m = 0; m <= gridSize - 1; m++) {
-      if ( board[n][m] === 'x' ) {
-
-      } else if (board[n][m] === 'o') {
-
-      }
-      if (countXH === 2) {
-      console.log('x wins');
-    } else if (countOH === 2) {
-      console.log('y wins');
-      }
-    }
   }
-
-
-
-  // //Vertical
-  // //column grow by 1, row doesnt grow
-  // if board[0][0] === x && board[1][0] === x && board[2][0] === x
-  // if board[0][1] === x && board[1][1] === x && board[2][1] === x
-  // if board[0][2] === x && board[1][2] === x && board[2][2] === x
-
-  countXV = 0;
-  countYV = 0;
-
-  for (o = 0; o <= gridSize - 1; o++) {
-    for (var p = 0; p <= gridSize - 1; p++) {
-      if ( board[o][p] === 'x' ) {
-        countXH = p + 1;
-      } else if (board[o][p] === 'o') {
-        countYH = p + 1;
-      }
-      if (countXV === 2) {
-      console.log('x wins');
-    } else if (countYV === 2) {
-      console.log('y wins');
-      }
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // //Diagonals
-  // //both grow evenly
-  // if board[0][0] === x && board[1][1] === x && board[2][2] === x
-  // //both grow opposite
-  // if board[0][2] === x && board[1][1] === x && board[2][0] === x
-  //
-
-
 }
 
 
-
-
-//
+//Reset Board / Refresh Page
+$('body > img').on('click', function reset () {
+  location.reload();
+});
--- readme.md
+++ readme.md
@@ -1,15 +1,37 @@
 
-**Instructions**
-The object of Tic Tac Toe is to get three in a row. The standard format for gameplay is based on aa three by three game board. But, because mine is awesome, you can play a grid as large as you want. The first player is known as X and the second is O. Players alternate placing Xs and Os on the game board until either opponent has three in a row or all nine squares are filled. If no one achieve 3 in a row, the stalemate is called a cat game.
+
+[Click here to sum my Tic Tac Toe Simulator in all it's AWESOMENESS!!](https://jozzzaa.github.io/tic-tac-toe/)
 
 
-**Features**
+# Instructions
 
--Responsive Design, optimised for Desktop or Mobile. In case you wanna play on the go.
--Parallax Background with interactivity through mouse interaction, fancy stuff!
--Random selection for who begins first, or it wouldn't be fair.
--Ability to reset match at any time, in case your a cheater and want to start before the match is over.
--Sexy scoring system allowing best of 3.
--Dynamic platform allowing board sizes of any size. Seriously who wants to play tic tac toe on a 10x10 board.
--Name Personalisation. In case you forget.
--Clear Interface to convey who's turn. In case your name wasn't clear enough.
+The object of Tic Tac Toe is to get three in a row. The standard format for gameplay is based on a three by three grid game board. But, because mine is awesome, you can play a grid as large as you want. The first player is known as X and the second is O. Players alternate placing Xs and Os on the game board until either opponent has three in a row or all nine squares are filled. If no one achieve 3 in a row, the stalemate is called a cat game.
+
+
+# Features
+
+- Responsive Design, optimised for Desktop or Mobile. In case you wanna play on the go.
+- Parallax Background with interactivity through mouse interaction, fancy stuff!
+- Random selection for who begins first, or it wouldn't be fair.
+- Ability to reset match at any time.
+- Sexy scoring system allowing best of 3. <<Coming Soon
+- Dynamic platform allowing board sizes of any size. Seriously who wants to play tic tac toe on a 10x10 board.
+- Name Personalisation.
+- Clear Interface to convey who's turn.
+- Amazing Favicon so people can find the tab
+
+# Formats / Workflows
+
+- HTML all contained in single container to  manage width of active user interface.
+- Intro Prompt and Game play held in additional wrappers with game display: none on load, this then switches once required user inputs have been entered. If required inputs, not entered, js will automatically apply default name values.
+- Board will default load 3 x 3 on canvas load, unless other number entered. Board will load within the canvas area
+- JS will load an identical array based board in the DOM and a winning array based on the board size input.
+- Squares clicked are primarily implemented on the DOM, this is then translated to the HTML based on the DOM board.
+- Scoring is built on mathematical principles of board expansion allowing scoring on all board sizes.
+- All functions are built dynamic with little hard coding allowing for flexibility within the game interface.
+
+# Difficulties / Unsolved Problems
+
+- Reset button utilises a function to reload the page, however my goal would be to regenerate the dom, and html div (squares) rather than a full page reload. This is required at the end of each match.
+- Scoring is not 100% dynamic for scoring on larger boards with bugs appearing.
+- So thankful to harry for helping with the scoring as I spend 24 hours rewriting and rewriting code with no luck.
